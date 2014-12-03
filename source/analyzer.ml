7b9d1491f69e24460a25a6fc37811a60bc1cba25
Daniel Maxson <dsm2157@columbia.edu>
2014-12-03 02:45:00 -0500

    Working on analyzer, got through compiler with TODOs left

--- source/analyzer.ml
+++ source/analyzer.ml
@@ -1,5 +1,5 @@
 open Ast
-
+open Sast
 
 (* A symbol table wich includes a parent symbol table
    and variables which are tuples of stings and Sast types *)
@@ -13,11 +13,66 @@ type environment = {
     local_scope : symbol_table;
 }
 
-let rec find_variable (scope : symbol_table) (name : String) : Sast.t option =
+let type_of_const (ac : Sast.a_constant) : Sast.t =
+  match ac with
+  | AInt(_, t) -> t
+  | AFloat(_, t) -> t
+  | ABool(_, t) -> t
+  | AString(_, t) -> t
+
+let rec type_of (ae : Sast.a_expr) : Sast.t =
+  match ae with
+  | AConstant(const) -> type_of_const const
+  | AId(_,_, t) -> t
+  | AUnop(_, _, t) -> t
+  | ABinop(_, _, _, t) -> t
+  | AAssign(expr) -> type_of expr
+
+
+let rec find_variable (scope : symbol_table) (name : string) : Sast.t option =
   try
     let (_, typ) = List.find (fun (s, _) -> s = name ) scope.variables in
     Some(typ)
-  with Not_Found -> None
+  with Not_found -> None
+
+
+(* Check to see if paran is important or not *)
+let mutate_or_add (scope : symbol_table) (name : string) (new_type : Sast.t) =
+  let typ = find_variable scope name in
+  match typ with
+  | Some(t) ->
+    (* filter name, t out of symbol_table.variables *)
+    scope.variables <- (name, new_type) :: (List.filter (fun (s, _) -> s <> name) scope.variables)
+  | None ->
+    scope.variables <- (name, new_type) :: scope.variables
+
+let require_int_float (expr : a_expr) (err : string) : int =
+  match type_of expr with
+  | TInt -> 0
+  | TFloat -> 1
+  | _ -> failwith err
+
+
+let require_bool (expr : a_expr) (err : string) : int =
+  match type_of expr with
+  | TBool -> 0
+  | _ -> failwith err
+
+
+let weak_eq_type (t1 : Sast.t) (t2 : Sast.t) =
+  true
+
+(*   let scope.variables =
+    scope.variables ::
+ *)
+
+  (*
+    Check to see if e1 is actually an ID, then annotate e2 and add this
+    assignment to the enviroment ( check the scope of the ID to add it to
+    the correct one)
+
+   *)
+
 
 
 (* Loads of useful utilities -- let me build them out
@@ -30,64 +85,66 @@ let rec find_variable (scope : symbol_table) (name : String) : Sast.t option =
 
 (* what is the : Sast.t_expr actually doing? *)
 
-let annotate_const (c : Ast.constant) : Sast.a_constant =
+let annotate_const (c : Ast.constant) : Sast.a_expr =
     match c with
-  | Int(n)    -> AInt(n, TInt)
-  | Float(f)  -> AFloat(f, TFloat)
-  | Bool(b)   -> ABool(b, TBool)
-  | String(s) -> AString(s, TString)
+  | Int(n) -> AConstant(AInt(n, TInt))
+  | Float(f) -> AConstant(AFloat(f, TFloat))
+  | Bool(b) -> AConstant(ABool(b, TBool))
+  | String(s) -> AConstant(AString(s, TString))
 
-and rec annotate_expr (e : Ast.expr) (env : environment) : Sast.a_expr =
+let rec annotate_expr (e : Ast.expr) (env : environment) : Sast.a_expr =
     match e with
-  | Constant(c)   -> annotate_const c
-  | Id(i, s)      ->
+  | Constant(c) -> annotate_const c
+  | Id(i, s) ->
       let typ =
         match s with
         | Local -> find_variable env.local_scope
         | Global -> find_variable env.global_scope
-      in (match typ with
-        | Some(x)   -> AId(i, s, x)
-        | None      -> fails_with("unrecognized identifier " ^ i ^ ".")
-      )
-  | Unop(e1, op)  ->
+      in AId(i, s, TInt)
+      (* TODO: FIX THIS WHY IS THIS NOT WORKING *)
+      (* (match typ with
+        | Some(x) -> AId(i, s, x)
+        | None -> fails_with "unrecognized identifier " ^ i ^ ".") *)
+  | Unop(op, e1) ->
       let ae1 = annotate_expr e1 env in
-      AUnop(ae1, TBool)
+      AUnop(op, ae1, type_of ae1)
   | Binop(e1, op, e2) ->
       let ae1 = annotate_expr e1 env
       and ae2 = annotate_expr e2 env in
 
-      let _, t1 = e1
-      and _, t2 = e2 in
-
-      (* This if the operation is anything besides =, != it requires that both
-         operands be integers or floats *)
-      if op <> Ast.op.Equal && op <> Ast.op.Neq &&
-         op <> Ast.op.And && op <> Ast.op.Or then
-        (let a = require_int_float e1 "left operand must be either integer or float" and
-         let b = require_int_float e2 "right operand must be either integer or float" in
-          if a + b = 0 then
-          ABinop(ae1, op, ae2, TInt)
-          else ABinop(ae1, op, ae2, TFloat))
-
-
-      else if op = Ast.op.And || op = Ast.op.Or then
-          (require_bool e1 "left operand must be a bool";
-           require_bool e2 "right operand must be a bool";
-          ABinop(ae1, op, ae2, TBool) )
-      else
-        if not (weak_eq_type t1 t2) then
-          fails_with "Type mismatch in comparison" else
+      (match op with
+        | Add | Sub | Mult | Div
+        | Neq | Lt | Leq | Gt | Geq ->
+          (let a = require_int_float ae1 "left operand must be either integer or float" and
+            b = require_int_float ae2 "right operand must be either integer or float" in
+             if a + b = 0 then
+             ABinop(ae1, op, ae2, TInt)
+            else ABinop(ae1, op, ae2, TFloat))
+        | And | Or ->
+          ignore (require_bool ae1 "left operand must be a bool");
+          ignore (require_bool ae2 "right operand must be a bool");
           ABinop(ae1, op, ae2, TBool)
+        | _ ->
+          if not (weak_eq_type (type_of ae1) (type_of ae2)) then
+          failwith "Type mismatch in comparison" else
+          ABinop(ae1, op, ae2, TBool))
+    | Assign(e1, e2) ->
+        let str, ret_value, scope =
+          match e1 with
+          | Id(str2, scope) -> str2, (annotate_expr e2 env), scope
+          | _ -> failwith "PLACEHOLDER"
+        in match scope with
+        (* TODO: Get the actual type to pass into mutate_or_add *)
+          | Local -> mutate_or_add env.local_scope str TInt; AAssign(ret_value)
+          | Global -> mutate_or_add env.global_scope str TInt; AAssign(ret_value)
+        (* ADD: add the case where the left is an array access *)
 
-
-
-
-let annotate_prog (p : Ast.program ) : Sast.t_program =
+(* let annotate_prog (p : Ast.program ) : Sast.t_program =
     let env  = create_env() in
     annotate_recipes p env
     annotate_stages p env
 
 
-
+ *)
 
 
--- source/sast.ml
+++ source/sast.ml
@@ -5,6 +5,7 @@ type t =
   | TFloat
   | TBool
   | TString
+  | TList
 
 type a_constant =
     AInt of int * t
@@ -12,18 +13,17 @@ type a_constant =
   | ABool of bool * t
   | AString of string * t
 
-
 type a_expr =
     AConstant of a_constant
   | AId of string * Ast.scope * t
-  | AUnop of Ast.op * a_expr
+  | AUnop of Ast.op * a_expr * t
   | ABinop of a_expr * Ast.op * a_expr * t
+  | AAssign of a_expr
 
 
 
 
-
-type t_program = {
+(* type t_program = {
     t_recipes = t_recipe list
     t_stage  = t_stage list
-}
+} *)
