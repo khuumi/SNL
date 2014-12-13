a76418ca431a5509d8e91a703e998c82e0832ce2
James Lin <jl3782@columbia.edu>
2014-12-12 21:22:08 -0500

    product java complete and edits to snl

--- examples/sample1/Recipe_does_hit_land.java
+++ examples/sample1/Recipe_does_hit_land.java
@@ -1,25 +1,21 @@
 public final class Recipe_does_hit_land{
 
-	private static int attacker_speed;
-	private static int defender_speed;
-	private static Boolean returnBoolean;
+    private static SNLObject attacker_speed;
+    private static SNLObject defender_speed;
+    private static SNLObject ret;
 
-	public static boolean perform(int attacker_speed_arg, int
-					defender_speed_arg){
+    public static SNLObject perform(SNLObject attacker_speed_arg, 
+        SNLObject defender_speed_arg){
 
-		attacker_speed = attacker_speed_arg;
-		defender_speed = defender_speed_arg;
+        attacker_speed = new SNLObject(attacker_speed_arg);
+        defender_speed = new SNLObject(defender_speed_arg);
 
-		Boolean returnBoolean = null;
-		returnBoolean = calc();
-		if(returnBoolean == null){
-			// call next helper
-		}
-		return returnBoolean.booleanValue();
-	}
+        calc();
+        return ret;
+    }
 
-	private static Boolean calc(){
-		return new Boolean(attacker_speed >= defender_speed);
-	}
+    private static void calc(){
+        ret = attacker_speed.geq(defender_speed);
+    }
 
 }
