572b8e543d45b3a4b256e4f84900f08098317a97
James Lin <jl3782@columbia.edu>
2014-12-13 22:31:40 -0500

    got --ouput_path fully functional

--- source/snl.ml
+++ source/snl.ml
@@ -1,5 +1,4 @@
 open Analyzer
-open Str
 
 type action = Expr | Stmt | Program | Java
 
@@ -19,19 +18,10 @@ let _ =
                                (Parser.program Scanner.tokenize lexbuf))
 
   | Java ->
-  (* see if file exists and remove if it is already there *)
-    let strlst = split (regexp "/") Sys.argv.(2) in
-    let path = if Array.length Sys.argv > 3
-            then List.assoc Sys.argv.(3) [("-path" ,"./"^Sys.argv.(4)^"/");]
-               else "java/" in
-     
-    let name = String.sub (List.tl strlst) 0 ((String.length (List.tl strlst))-4) in
-    let pname = path^name^".java" in
-
      (* see if file exists and remove if it is already there *)
-     let strlst = split (regexp "/") Sys.argv.(2) in 
-     let name = (String.sub (List.tl strlst) 0
-                   ((String.length Sys.argv.(2)) - 4)) in
+     let strlst = Str.split (Str.regexp "/") Sys.argv.(2) in
+     let tail = List.hd (List.rev strlst) in
+     let name = String.sub tail 0 ((String.length tail) - 4) in
      let pname = if Array.length Sys.argv > 3 && Sys.argv.(3) = "--output_path"
                  then Sys.argv.(4) ^ "/" ^ name ^ ".java"
                  else name ^ ".java" in
